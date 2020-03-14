307beaae174b50b6b9ac126f9614a17dfc8abd00
bryanl <bryanliles@gmail.com>
2020-03-14 08:12:03 -0400

    add integration test for confg set-udi

--- internal/integration/config_test.go
+++ internal/integration/config_test.go
@@ -110,3 +110,132 @@ func Test_sheaf_config_add_image(t *testing.T) {
 		})
 	}
 }
+
+func Test_sheaf_config_set_udi(t *testing.T) {
+
+	cases := []struct {
+		name     string
+		existing []sheaf.UserDefinedImage
+		udi      udi
+		wanted   []sheaf.UserDefinedImage
+	}{
+		{
+			name: "set user defined image",
+			udi: udi{
+				APIVersion: "example.com/v1",
+				Kind:       "Resource",
+				JSONPath:   "{.spec.image}",
+			},
+			wanted: []sheaf.UserDefinedImage{
+				{
+					APIVersion: "example.com/v1",
+					Kind:       "Resource",
+					JSONPath:   "{.spec.image}",
+					Type:       sheaf.SingleResult,
+				},
+			},
+		},
+		{
+			name: "set user defined image with existing",
+			existing: []sheaf.UserDefinedImage{
+				{
+					APIVersion: "example.com/v2",
+					Kind:       "Resource",
+					JSONPath:   "{.spec.image}",
+					Type:       sheaf.SingleResult,
+				},
+			},
+			udi: udi{
+				APIVersion: "example.com/v1",
+				Kind:       "Resource",
+				JSONPath:   "{.spec.image}",
+			},
+			wanted: []sheaf.UserDefinedImage{
+				{
+					APIVersion: "example.com/v1",
+					Kind:       "Resource",
+					JSONPath:   "{.spec.image}",
+					Type:       sheaf.SingleResult,
+				},
+				{
+					APIVersion: "example.com/v2",
+					Kind:       "Resource",
+					JSONPath:   "{.spec.image}",
+					Type:       sheaf.SingleResult,
+				},
+			},
+		},
+		{
+			name: "update existing image",
+			existing: []sheaf.UserDefinedImage{
+				{
+					APIVersion: "example.com/v1",
+					Kind:       "Resource",
+					JSONPath:   "{.spec.image}",
+					Type:       sheaf.SingleResult,
+				},
+			},
+			udi: udi{
+				APIVersion: "example.com/v1",
+				Kind:       "Resource",
+				JSONPath:   "{.spec.images}",
+				Type:       "multiple",
+			},
+			wanted: []sheaf.UserDefinedImage{
+				{
+					APIVersion: "example.com/v1",
+					Kind:       "Resource",
+					JSONPath:   "{.spec.images}",
+					Type:       sheaf.MultiResult,
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			withWorkingDirectory(t, func(wd string) {
+				bundleDir := initBundle(t, testHarness, "integration", wd)
+
+				var config sheaf.BundleConfig
+				configFile := filepath.Join(bundleDir, "bundle.json")
+
+				if len(tc.existing) > 0 {
+					readJSONFile(t, configFile, &config)
+					config.UserDefinedImages = tc.existing
+					writeJSONFile(t, configFile, config)
+				}
+
+				args := append([]string{"config", "set-udi"}, tc.udi.toArgs()...)
+
+				err := testHarness.runSheaf(bundleDir, defaultSheafRunSettings, args...)
+				require.NoError(t, err)
+
+				readJSONFile(t, configFile, &config)
+
+				require.Equal(t, tc.wanted, config.UserDefinedImages)
+			})
+		})
+	}
+}
+
+type udi struct {
+	APIVersion string
+	Kind       string
+	JSONPath   string
+	Type       string
+}
+
+func (u udi) toArgs() []string {
+	args := []string{
+		"--api-version", u.APIVersion,
+		"--kind", u.Kind,
+		"--json-path", u.JSONPath,
+	}
+
+	if u.Type != "" {
+		args = append(args, "--type", u.Type)
+	}
+
+	return args
+}
